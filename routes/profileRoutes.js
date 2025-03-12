4bcf88aeef1550fd5e36b821cc898becb943d3bf
vasylhryts <vasyl.hryts@requestum.com>
2025-03-12 14:54:51 +0200

    added editing dreams

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -11,17 +11,50 @@ router.get("/", isAuthenticated, async (req, res) => {
     res.render("profile", { user, dreams });
   } catch (error) {
     console.error(error);
-    res.status(500).send("Internal Server Error");
+    return res.status(500).send("Internal Server Error");
   }
 });
 
 router.post("/delete/:id", isAuthenticated, async (req, res) => {
   try {
     await Dream.findOneAndDelete({ _id: req.params.id, author: req.session.user._id });
-    res.redirect("/profile");
+    return res.redirect("/profile");
   } catch (error) {
     console.error(error);
-    res.status(500).send("Internal Server Error");
+    return res.status(500).send("Internal Server Error");
+  }
+});
+
+router.get("/edit_dream/:id", isAuthenticated, async (req, res) => {
+  try {
+    const dream = await Dream.findOne({ _id: req.params.id, author: req.session.user._id });
+
+    if (!dream) {
+      return res.redirect("/profile");
+    }
+
+    return res.render("editDream", { dream });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Internal Server Error");
+  }
+});
+
+router.post("/edit_dream/:id", isAuthenticated, async (req, res) => {
+  try {
+    await Dream.findOneAndUpdate(
+      { _id: req.params.id, author: req.session.user._id },
+      {
+        title: req.body.title,
+        description: req.body.description,
+        categories: req.body.categories,
+      }
+    );
+
+    return res.redirect("/profile");
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Internal Server Error");
   }
 });
 
