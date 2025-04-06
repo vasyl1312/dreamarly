4749140504b3347523ab655990531130e9965c2e
vasylhryts <vasyl.hryts@requestum.com>
2025-04-06 21:20:36 +0300

    simple rating

--- routes/allDreamsRoutes.js
+++ routes/allDreamsRoutes.js
@@ -4,12 +4,37 @@ const Dream = require("../models/dreams");
 
 router.get("/", async (req, res) => {
   try {
-    const dreams = await Dream.find().sort({ date: -1 });
+    const sortOption = req.query.sort || "top";
+    let dreams;
+
+    if (sortOption === "latest") {
+      dreams = await Dream.find().sort({ date: -1 });
+    } else if (["very_cool", "pleasant", "funny", "mind_blown", "weird"].includes(sortOption)) {
+      const sortKey = `reactions.${sortOption}`;
+      dreams = await Dream.find().sort({ [sortKey]: -1, date: -1 });
+    } else {
+      dreams = await Dream.aggregate([
+        {
+          $addFields: {
+            reactionCount: {
+              $add: [
+                "$reactions.very_cool",
+                "$reactions.pleasant",
+                "$reactions.funny",
+                "$reactions.mind_blown",
+                "$reactions.weird",
+              ],
+            },
+          },
+        },
+        { $sort: { reactionCount: -1, date: -1 } },
+      ]);
+    }
 
     const alert = req.session.alert || { type: "", message: "" };
     req.session.alert = null;
 
-    res.render("dreams/allDreams", { dreams, alert });
+    res.render("dreams/allDreams", { dreams, alert, sort: sortOption });
   } catch (error) {
     console.error(error);
     return res.status(500).send("Internal Server Error");
--- routes/genresRoutes.js
+++ routes/genresRoutes.js
@@ -5,12 +5,48 @@ const Dream = require("../models/dreams");
 router.get("/:category", async (req, res) => {
   try {
     const category = req.params.category;
-    const dreams = await Dream.find({ categories: category }).populate("author");
+    const sortOption = req.query.sort || "top";
+
+    let dreams;
+
+    if (sortOption === "latest") {
+      dreams = await Dream.find({ categories: category }).populate("author").sort({ date: -1 });
+    } else if (["very_cool", "pleasant", "funny", "mind_blown", "weird"].includes(sortOption)) {
+      const sortKey = `reactions.${sortOption}`;
+      dreams = await Dream.find({ categories: category })
+        .populate("author")
+        .sort({ [sortKey]: -1, date: -1 });
+    } else {
+      // Загальна кількість реакцій (без дизлайків)
+      dreams = await Dream.aggregate([
+        { $match: { categories: category } },
+        {
+          $addFields: {
+            reactionCount: {
+              $add: [
+                "$reactions.very_cool",
+                "$reactions.pleasant",
+                "$reactions.funny",
+                "$reactions.mind_blown",
+                "$reactions.weird",
+              ],
+            },
+          },
+        },
+        { $sort: { reactionCount: -1, date: -1 } },
+      ]);
+
+      // Після aggregate потрібен manual populate
+      const dreamsPopulated = await Dream.populate(dreams, { path: "author" });
+      dreams = dreamsPopulated;
+    }
+
     res.render("genres/genre", {
       category,
       dreams,
       user: req.user,
       alert: req.session.alert || { type: "", message: "" },
+      sort: sortOption,
     });
 
     req.session.alert = null;
